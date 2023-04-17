05be8312a598ad52b942652012556a2cb52fc4b8
Egor Lappo <elappo@stanford.edu>
2023-04-17 01:25:59 -0700

    hix fix

--- nix/hix.nix
+++ nix/hix.nix
@@ -2,11 +2,11 @@
   name = "impute";
   compiler-nix-name = "ghc944"; # Version of GHC to use
 
-  crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
-    p.mingwW64
-  ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-    p.musl64
-  ]);
+  # crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
+  #   p.mingwW64
+  # ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+  #   p.musl64
+  # ]);
 
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
