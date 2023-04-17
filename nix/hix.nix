70d3b820835df845b43f8f8dc0e1bdb2f961cc0b
Egor Lappo <elappo@stanford.edu>
2023-04-17 01:34:05 -0700

    hix update

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   name = "impute";
-  compiler-nix-name = "ghc944"; # Version of GHC to use
+  compiler-nix-name = "ghc961"; # Version of GHC to use
 
   # crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
   #   p.mingwW64
